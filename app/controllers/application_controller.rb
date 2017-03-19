c6a413dfc91e99de33183380d95bf6e01b9bb3c1
Maureen Riley & Ryan Landgren <pair+moriley5.rklangrin@devbootcamp.com>
2017-03-19 11:52:34 -0500

    Complete login & register routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  helper_method :logged_in?
+
+  def current_user
+    @_current ||= User.find_by(id: session[:user_id]) if logged_in?
+  end
+
+  helper_method :current_user
+
+  def authorize_to_view(user_id)
+    if current_user.id != user_id
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def redirect_home
+    redirect_to root_path if !logged_in?
+  end
 end
