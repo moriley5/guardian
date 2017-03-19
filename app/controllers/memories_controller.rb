3bc82bd85ea9bfd8fc75bdb2beaa6f963c8d7a39
JD Hiudt <jacob.d.hiudt@gmail.com>
2017-03-19 00:34:40 -0500

    controller set up and routes

---
+++ app/controllers/memories_controller.rb
@@ -0,0 +1,16 @@
+class MemoriesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #login" do
+    it "returns http success" do
+      get :login
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #logout" do
+    it "returns http success" do
+      get :logout
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
