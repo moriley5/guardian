3509dca039793190b478ea982a837c1ddb55340b
JD Hiudt <jacob.d.hiudt@gmail.com>
2017-03-19 01:47:04 -0500

    geneated controllers-created views and specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :delete]
+
+  resources :guardian_profiles, only: [:new, :create, :show, :edit, :update] do
+    resources :memories, only: [:new, :create, :edit, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
