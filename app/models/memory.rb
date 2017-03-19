fa8117e5f7eac03ec594f3dd5628eff1f3b64138
JD Hiudt <email": null>
2017-03-19 14:19:41 -0500

    add audio file to assets folder to test path, working - seed file, updated memory audio validations

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,9 +1,12 @@
 class Memory < ApplicationRecord
   belongs_to :guardian_profile, foreign_key: :guardian_id
 
-  validates :guardian_id, :audio_recording, :description, presence: true
-
   has_attached_file :audio
+
+  validates :guardian_id, :audio_recording, :description, presence: true
+  validates_attachment :audio, presence: true, content_type: {content_type: "audio/mpeg"}, size: { in: 0..100.kilobytes }
+
+
 
   def formatted_date
     self.date.strftime("%m/%d/%Y")
