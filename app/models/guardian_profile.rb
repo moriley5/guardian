7006ca07a050ef4d8594aec427cfcc8c0a1d528d
JD Hiudt <email": null>
2017-03-19 16:29:01 -0500

    bug with paperclip images, reverted back to image as text field for now

--- app/models/guardian_profile.rb
+++ app/models/guardian_profile.rb
@@ -2,8 +2,5 @@ class GuardianProfile < ApplicationRecord
   has_many :memories, foreign_key: :guardian_id
   belongs_to :user
 
-  validates :user_id, :title, presence: true
-  validates_attachment :image, :presence => true,
-    :content_type => {:content_type => "image/jpeg" },
-    :size => { in: 0..500.kilobytes }
+  validates :user_id, :title, :image, presence: true
 end
