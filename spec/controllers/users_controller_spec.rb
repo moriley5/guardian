fb9f3337a29f37fd9c90d1fd995b066af0c54690
JD Hiudt <email": null>
2017-03-19 13:23:23 -0500

    made certain tests pending

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe UsersController, type: :controller do
 
 
   describe "GET #show" do
-    it "returns http success" do
+    xit "returns http success" do
       get :show
       expect(response).to have_http_status(:success)
     end
