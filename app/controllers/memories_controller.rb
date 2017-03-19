3ef106e163467cd114d900addc8633839e011bab
Maureen Riley & Ryan Landgren <pair+moriley5.rklangrin@devbootcamp.com>
2017-03-19 13:59:48 -0500

    Create form to add a memory

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,13 +1,29 @@
 class MemoriesController < ApplicationController
   def new
+    @guardian = GuardianProfile.find_by(id: params[:guardian_id])
+    @memory = Memory.new
   end
 
   def create
+    @memory = Memory.new(memory_params)
+
+    if @memory.save
+      redirect_to guardian_profile
+    else
+      @errors = @memory.errors_full_messages
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def memory_params
+    params.require(:memory).permit(:guardian_id, :description, :audio_recording, :audio, :date)
   end
 end
