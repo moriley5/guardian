bdc6453823a962d96593216641f918bc5224c05d
JD Hiudt <email": null>
2017-03-21 10:28:23 -0500

    applied style and modified some routes

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,12 +1,12 @@
 class Memory < ApplicationRecord
   belongs_to :guardian_profile, foreign_key: :guardian_id
 
-  has_attached_file :audio
+  has_attached_file :audio, :content_type => ["audio/x-m4a", "audio/m4a", "audio/mp3"]
 
   validates :guardian_id, :description, presence: true
   validates_attachment :audio, :presence => true,
     :size => { in: 0..500.kilobytes }
-  validates_attachment_content_type :audio, :content_type => ["audio/x-m4a", "audio/m4a"]
+  validates_attachment_content_type :audio, :content_type => ["audio/x-m4a", "audio/m4a", "audio/mp3"]
 
 
 
