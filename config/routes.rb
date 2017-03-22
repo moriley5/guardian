9aaffaf4160961184140dc978e20b359dac520ac
Chase Wessman & Maureen Riley & Ryan Landgren <pair+cjwess.moriley5.rklangrin@devbootcamp.com>
2017-03-22 18:20:52 -0500

    fixes sessions controller. Edits link_to login and register to use rails paths

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   root "sessions#new"
   resources :users, only: [:index, :new, :create, :show]
   resources :sessions, only: [:new, :create]
+  get '/logout', to: 'sessions#destroy', as: :logout
+
   resources :guardian_profiles do
     resources :memories, only: [:new, :create, :edit, :update, :destroy]
     resources :notes, only: [:new, :destroy, :create, :edit, :update]
   end
-
-  get '/logout', to: 'sessions#destroy', as: :logout
 end
 
