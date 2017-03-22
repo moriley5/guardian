6d5c64fde4a60f0136890b67e3589f47119fbc15
Chase Wessman & Maureen Riley & Ryan Landgren <pair+cjwess.moriley5.rklangrin@devbootcamp.com>
2017-03-22 17:15:37 -0500

    fixes downcase for email and name registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :email, :password_digest, presence: true
   validates :email, uniqueness: true
 
+  before_save :downcase_fields
+
   has_secure_password
+
+  def downcase_fields
+    self.first_name.downcase!
+    self.last_name.downcase!
+    self.email.downcase!
+  end
 end
