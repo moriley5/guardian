624e5a7fa86a6a37cde9a0950fb93ee11b2d0f13
Chase Wessman & Maureen Riley & Ryan Landgren <pair+cjwess.moriley5.rklangrin@devbootcamp.com>
2017-03-23 15:50:54 -0500

    change from desc to asc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
     authorize_to_view(@user.id) if @user
     @user = current_user
     @guardian = GuardianProfile.new
-    @guardians = @user.guardian_profiles.order(created_at: :asc)
+    @guardians = @user.guardian_profiles.order(created_at: :desc)
   end
 
   private
